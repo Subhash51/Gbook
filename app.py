667130f962ac9ac72c4d446ee02c38e359e10601
Mayank7603 <94745992+Mayank7603@users.noreply.github.com>
2024-03-21 23:39:39 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('register.html')
+
+@app.route('/login')
+def index2():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
